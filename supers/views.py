5f232e2cc691bbc249a92c5cfcbc23bfac985bc6
irishandyesiam <nathan.irish@gmail.com>
2022-08-26 15:33:54 -0500

    ERD complete.

--- supers/views.py
+++ supers/views.py
@@ -1,8 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import SuperSerializer
+from .models import Super
+from supers import serializers
+
 
 @api_view(['GET'])
 def supers_list(request):
-
-
-    return Response('ok')
+    supers = Super.objects.all()
+    serializer = SuperSerializer(supers, many=True)
+    return Response(serializer.data)
