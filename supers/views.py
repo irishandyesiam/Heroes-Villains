43281222f60ae7e1004139878e92d2e4bb70d2ff
irishandyesiam <nathan.irish@gmail.com>
2022-08-26 16:03:03 -0500

    Updated ERD.

--- supers/views.py
+++ supers/views.py
@@ -1,12 +1,34 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import SuperSerializer
 from .models import Super
 from supers import serializers
 
-
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
-    supers = Super.objects.all()
-    serializer = SuperSerializer(supers, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        supers = Super.objects.all()
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def super_detail(request, pk):
+    super = get_object_or_404(super, pk=pk)
+    if request.method == 'GET':
+        serializer = SuperSerializer(super)
+        return Response(serializer.data, status=status.HTTP_200_OK)  
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(super, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
