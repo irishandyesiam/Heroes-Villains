6021e01a6bd9de4123aca9b1a59ba213944d4b5b
irishandyesiam <nathan.irish@gmail.com>
2022-08-30 10:58:56 -0500

    Revised function supers list with If,ELIF,ELSE.

--- supers/views.py
+++ supers/views.py
@@ -5,26 +5,33 @@ from rest_framework import status
 from .serializers import SuperSerializer
 from .models import Super
 from supers import serializers
+from .models import SuperType
 
 @api_view(['GET', 'POST'])
 def supers_list(request):
     if request.method == 'GET':
-
         select_type = request.query_params.get('type')
-        print(select_type)
         supers = Super.objects.all()
-
         if select_type:
             supers = supers.filter(super_type__type=select_type)
-
         serializer = SuperSerializer(supers, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
+        return Response(serializer.data, status=status.HTTP_201_CREATED)   
+    else:
+        super_types = Super.objects.all()
+        custom_response_dictionary = {}
+        for super_type in super_types:
+            supers = SuperType.objects.filter(super_type_id=super_type.id)
+            serializer = SuperSerializer(supers, many=True)
+            custom_response_dictionary[super_type.type] = {
+                "heroes": serializer.type,
+                "villians": serializer.type
+            }
+        return Response(custom_response_dictionary, status=status.HTTP_200_OK)
 @api_view(['GET', 'PUT', 'DELETE'])
 def super_detail(request, pk):
     super = get_object_or_404(Super, pk=pk)
